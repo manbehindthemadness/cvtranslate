c01a1d9d3557c27d2c79d39bb9e7d0a959d28659
Kevin Eales <manbehindthemadness@gmail.com>
2022-11-14 06:48:30 -0500

    Added source data, arguments, and requirements.txt

--- main.py
+++ main.py
@@ -7,10 +7,31 @@ import os
 import csv
 import datetime
 from pathlib import Path
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--baseline', action="store_true", help='Baseline normalization average', default=1)
+parser.add_argument('-ema', '--exponential_moving_average', store="store_true", help='exponential moving average', default=20)
+parser.add_argument('-tt', '--time_to_ticks', action="store_true", help='Process timestamps in ticks or human readable', default=False)
+
+
+args = parser.parse_args()
 
 
 timeframes = ['15MINUTE', '30MINUTE', '1HOUR', '4HOUR', '1DAY']
+AL = args.exponential_moving_average
+BL = args.baseline
+TT = args.time_to_ticks
 
+for arg in [AL, BL]:
+    if not isinstance(arg, int):
+        raise TypeError
+if not isinstance(TT, bool):
+    raise TypeError
+if BL < AL:
+    print('baseline cannot be less than moving average')
+    raise ValueError
 
 here = Path(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 models = here / Path('models')
@@ -48,15 +69,13 @@ def string_to_ticks(timestamp: str) -> int:
     """
     This will convert our timestamps into standard ticks format.
     """
-    d = datetime.datetime.strptime(timestamp, "%Y-%m-%d-%H-%M")
-    t0 = datetime.datetime(1970, 1, 1,
-                           # tzinfo=datetime.timezone.utc
-                           )
-    ticks = (d - t0).total_seconds()
+    _d = datetime.datetime.strptime(timestamp, "%Y-%m-%d-%H-%M")
+    t0 = datetime.datetime(1970, 1, 1)
+    ticks = (_d - t0).total_seconds()
     return int(ticks)
 
 
-def translate_fields(fields: dict) -> dict:
+def translate_fields(fields: [dict, list]) -> tuple:
     """
     This will translate our field data from:
         {'stamp': '2021-07-18-01-53', 'bar': [0.91, 0.91, 0.84, 0.84, 1.443], 'alts': {'ada': 1.9, 'busd': 0.46, 'bnb': 0.66, 'trx': 0.66, 'doge': 1.86, 'xrp': 0.24}}
@@ -64,22 +83,57 @@ def translate_fields(fields: dict) -> dict:
         {'timestamp': val, 'open': val, 'high': val, 'low': val, 'close': val, 'volume': val, 'diversity': val}
     """
     f = fields
-    o, c, h, l, v = f['bar']
-    d = len(f['alts'])
-    timestamp = f['stamp']
+    if isinstance(fields, dict):
+        _o, _c, _h, _l, _v = f['bar']
+        _d = len(f['alts'])
+        timestamp = f['stamp']
+    elif isinstance(fields, list):
+        timestamp, _o, _h, _l, _c, _v, _d = fields
+    else:
+        raise ValueError
     try:
         timestamp = string_to_ticks(timestamp)
-    except ValueError:
+    except (ValueError, TypeError):
         pass
     result = {
         'timestamp': timestamp,
-        'open': o,
-        'high': h,
-        'low': l,
-        'close': c,
-        'volume': v,
-        'diversity': d,
-    }
+        'open': _o,
+        'high': _h,
+        'low': _l,
+        'close': _c,
+        'volume': _v,
+        'diversity': _d,
+    }, [
+        _o, _h, _l, _c
+    ]
+    return result
+
+
+def normalize_quotes(_quotes: list, average_length: int, average_base: int):
+    """
+    This will produce a moving average of the incoming data, then subtract it from itself, so we have a nice
+    horizontal chart plot.
+    """
+
+    def ema(_q, al) -> np.ndarray:
+        """
+        Yup EMA..
+        """
+        ret = np.cumsum(_q, dtype=float)
+        ret[al:] = ret[al:] - ret[:-al]
+        ays = ret[al - 1:] / al
+        padding = np.subtract(al, 1)
+        average = np.pad(ays, (padding, 0))
+        average = np.array(average)
+        return average
+
+    _qu = np.transpose(_quotes)
+    transposed_averages = list()
+    for _q in _qu:
+        ma = ema(_q, average_base)
+        normal = ema(_q, average_length)
+        transposed_averages.append(np.subtract(normal, ma))
+    result = np.transpose(transposed_averages)
     return result
 
 
@@ -102,12 +156,41 @@ for idx, m_file in enumerate(model_files):
         fieldnames = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'diversity']
         with open(output_file, 'w', newline='') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            quotes = list()
             for quote in frame_data:
                 try:
-                    row = translate_fields(quote)
+                    row, quote = translate_fields(quote)
+                    writer.writerow(row)
+                    quotes.append(quote)
+                except ValueError as err:
+                    print(row, '\n', err)
+                    pass
+            csv_file.close()
+        output_name = f'output/{output_files[idx]}_{timeframe}_PROCESSED{ext}'
+        output_file = Path(output_name)
+        normalized_quotes = normalize_quotes(
+            quotes,
+            AL,
+            BL,  # len(quotes)
+        )
+
+        if not output_file.is_file():
+            output_file.touch()
+        with open(output_file, 'w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for quote, normalized_quote in zip(frame_data, normalized_quotes):
+                q, _ = translate_fields(quote)
+                t = q['timestamp']
+                v = q['volume']
+                d = q['diversity']
+                o, h, l, c = normalized_quote
+                row = [t, o, h, l, c, v, d]
+                try:
+                    row, _ = translate_fields(row)
                     writer.writerow(row)
                 except ValueError as err:
                     print(row, '\n', err)
                     pass
             csv_file.close()
-
